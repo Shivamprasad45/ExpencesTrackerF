66f19810dd5968f0bedce7cc82a0f3a96c1746bc
Shivam gond <codewithharry35434@gmail.com>
2025-07-28 16:48:31 +0530

    niginx setu

--- lib/features/Premium/premiuemapi.ts
+++ lib/features/Premium/premiuemapi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const expensesPremiumApi = createApi({
   reducerPath: "expensesPremiumApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://13.203.138.162:5000/api/Premium",
+    baseUrl: "http://13.203.138.162/api/Premium",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("authToken");
       if (token) {
